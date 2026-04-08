72bfffaff17f3800ba4cbeec8198d87a048047b1
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2026-04-07 19:12:11 -0700

    Mini-Project: World Happiness Pipeline - Task 3 Visualizations

--- assignments_01/project_01.py
+++ assignments_01/project_01.py
@@ -2,6 +2,8 @@ from prefect import task, flow, get_run_logger
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Task 1: Load Multiple Years of Data
 
@@ -72,11 +74,70 @@ def compute_descriptive_stats(df):
         "yearly_mean": yearly_mean,
         "regional_mean": regional_mean
     }
+    
+# Task 3: Visual Exploration
+@task
+def create_visualizations(df):
+    logger = get_run_logger()
+    base_dir = Path(__file__).resolve().parent
+    output_dir = base_dir / "outputs"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    # Histogram
+    plt.figure(figsize=(8, 5))
+    plt.hist(df["Happiness score"], bins = 20, color="green", edgecolor="black")
+    plt.title("Distribution of Happiness Scores")
+    plt.xlabel("Happiness Score")
+    plt.ylabel("Frequency")
+    
+    output_file = output_dir / "happiness_histogram.png"
+    plt.savefig(output_file)
+    plt.close()
+    logger.info(f"Histogram saved to: {output_file}")
+    
+    # Boxplot
+    plt.figure(figsize=(10, 5))
+    sns.boxplot(x="year", y="Happiness score", data=df)
+    plt.title("Happiness Score Distribution by Year")
 
+    boxplot_path = output_dir / "happiness_by_year.png"
+    plt.savefig(boxplot_path)
+    plt.close()
+    logger.info(f"Boxplot saved to: {boxplot_path}")
+    
+    # Scatter: GDP vs Happiness
+    plt.figure(figsize=(8, 5))
+    plt.scatter(df["GDP per capita"], df["Happiness score"])
+    plt.title("GDP per Capita vs Happiness Score")
+    plt.xlabel("GDP per capita")
+    plt.ylabel("Happiness Score")
+
+    scatter_path = output_dir / "gdp_vs_happiness.png"
+    plt.savefig(scatter_path)
+    plt.close()
+    logger.info(f"Scatter plot saved to: {scatter_path}")
+    
+    # Correlation Heatmap
+    plt.figure(figsize=(12, 8))
+
+    numeric_df = df.select_dtypes(include="number")
+    corr_matrix = numeric_df.corr()
+    sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", fmt=".2f", annot_kws={"size": 9})
+    plt.title("Correlation Heatmap", pad=12)
+    plt.xticks(rotation=45, ha="right")
+    plt.yticks(rotation=0)
+    plt.tight_layout()
+
+    heatmap_path = output_dir / "correlation_heatmap.png"
+    plt.savefig(heatmap_path, bbox_inches="tight")
+    plt.close()
+    logger.info(f"Heatmap saved to: {heatmap_path}")
+    
 @flow
 def happiness_pipeline():
     df = load_and_merge_data()
     compute_descriptive_stats(df)
+    create_visualizations(df)
     
 if __name__ == "__main__":
     happiness_pipeline()
